00c7d553c619bcc1fad064fa8a2cbf5b83533244
mia-moran <mariamoranluaces@gmail.com>
2021-10-21 13:39:38 +0200

    project-configurated

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
